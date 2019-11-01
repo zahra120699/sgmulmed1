8239649b2106045f07464dde286dc3a2a263a411
zahra120699 <57255753+zahra120699@users.noreply.github.com>
2019-11-01 18:05:34 +0700

    upload1

---
+++ sgprogramming.m
@@ -0,0 +1,128 @@
+%menghitung luas layang-layang
+d1 = input('Masukan Diagonal 1 : ')
+d2 = input('Masukan Diagonal 2 : ')
+
+luas = (d1*d2)/2
+
+disp("luas layang-layang "+luas)
+
+%menghitung keliling layang-layang
+a = input('Masukan sisi a: ')
+b = input('Masukan sisi b: ')
+
+keliling = 2*(a+b)
+
+disp("keliling layang-layang "+keliling)
+
+%menghitung luas trapesium
+t = input('Masukan tinggi : ')
+s1 = input('Masukan sisi 1 : ') 
+s2 = input('Masukkan sisi 2 : ')
+
+luas = ((s1+s2)*t)/2
+
+disp("luas trapesium "+luas)
+
+%menghitung keliling trapesium
+a = input('Masukan sisi a: ')
+b = input('Masukan sisi b: ')
+c = input('Masukan sisi c: ')
+d = input('Masukan sisi d: ')
+
+keliling = a+b+c+d
+
+disp("keliling trapesium "+keliling)
+
+%menghitung luas jajar genjang
+a = input('Masukan alas: ')
+t = input('Masukan tinggi: ')
+
+luas = a*t
+
+disp("luas jajar genjang "+luas)
+
+%menghitung keliling jajar genjang
+a = input('Masukan sisi a: ')
+b = input('Masukan sisi b: ')
+
+keliling = 2*(a+b)
+
+disp("keliling jajar genjang "+keliling)
+
+%menghitung luas segitiga
+a = input('Masukan alas: ')
+t = input('Masukan tinggi: ')
+
+luas = a*t/2
+
+disp("luas segitiga "+luas)
+
+%menghitung keliling segitiga
+a = input('Masukan sisi a: ')
+b = input('Masukan sisi b: ')
+c = input('Masukan sisi c: ')
+
+keliling = a+b+c
+
+disp("keliling segitiga "+keliling)
+
+%menghitung luas belah ketupat
+d1 = input('Masukan Diagonal 1 : ')
+d2 = input('Masukan Diagonal 2 : ')
+
+luas = (d1*d2)/2
+
+disp("luas belah ketupat "+luas)
+
+%menghitung keliling belah ketupat
+s = input('Masukan sisi : ')
+
+keliling = 4*s
+
+disp("keliling belah ketupat "+keliling)
+
+%menghitung luas lingkaran
+r = input('Masukan jari-jari : ')
+a = input('Masukan phi: ')
+
+luas = a*r*r
+
+disp("luas lingkaran "+luas)
+
+%menghitung keliling lingkaran
+r = input('Masukan jari-jari : ')
+a = input('Masukan phi: ')
+
+luas = 2*r*a
+
+disp("keliling lingkaran "+keliling)
+
+%menghitung luas persegi panjang
+p = input('Masukan panjang : ')
+l = input('Masukan lebar : ')
+
+luas = p*l
+
+disp("luas persegi panjang "+luas)
+
+%menghitung keliling persegi panjang
+p = input('Masukan panjang : ')
+l = input('Masukan lebar : ')
+
+keliling = 2*(p+l)
+
+disp("keliling persegi panjang "+keliling)
+
+%menghitung luas persegi
+s = input('Masukan sisi : ')
+
+luas = s*s
+
+disp("luas persegi "+luas)
+
+%menghitung keliling persegi
+s = input('Masukan sisi : ')
+
+keliling = 4*s
+
+disp("keliling persegi "+keliling)
